8afe2125ddc969b09885c4882cecadeda1c79e78
dapoco <cadizd@proton.me>
2024-12-01 13:17:34 -0600

    Added pre-written classes from earlier web crawling project.

---
+++ HTTPResponse.py
@@ -0,0 +1,108 @@
+from PatternExtractor import PatternExtractor
+
+class HTTPResponse():
+    HTML_DELIMITERS = ["<!doctype html>", "<!DOCTYPE html>"]
+    CHAR_SET_DEFAULT = "utf-8"
+    BUFFSIZE_DEFAULT = 8
+    CHARSET_SUBHEADER_CATCHER = r".*[cC]harset=(.*)"
+
+    def __init__(self, url: str, data: bytes):
+        assert isinstance(data, bytes)
+        assert isinstance(url, str)
+        self._data_bytes = data
+        self._data_str = self._decode(data, HTTPResponse.BUFFSIZE_DEFAULT)
+        
+        # Public instance vars
+        self.url = url
+        self.status_code = self._get_status_code(self._data_str)
+        self.headers = self._get_headers(self._data_str)
+        self.content = self._get_content(self._data_str)
+
+    def _get_status_code(self, data_str):
+        ''' Return the status code of the response.'''
+        assert isinstance(data_str, str)
+        first_car_ret_ind = data_str.find("\r\n")
+        from twisted.internet import reactor
+        stat_msg = data_str[0:first_car_ret_ind]
+        stat_code = int(stat_msg.split(" ")[1]) # HTTP/$version $status $message
+
+    def _get_headers(self, data_str):
+        ''' Return the header content (with error code) of the response.'''
+        assert data_str != None
+        assert isinstance(data_str, str)
+
+        # Headers are always bounded before a double carriage return
+        headers_n_status= data_str.split("\r\n\r\n")[0]
+        headers = headers_n_status[1:len(headers_n_status)]
+        return headers
+   
+    def _get_content(self, data_str) -> str:
+        assert data_str != None
+        assert isinstance(data_str, str)
+
+        # Content is always past first double carriage return (headers)
+        content = data_str.split("\r\n\r\n")[1]
+        return content
+
+    def _decode(self, data_bytes: bytes, buffsize: int):
+        ''' Decode byte data depending on the encoding.'''
+        assert isinstance(data_bytes, bytes)
+        assert isinstance(buffsize, int)
+        assert buffsize > 0
+        charset = HTTPResponse.CHAR_SET_DEFAULT # should be utf-8
+        decoded_data = ""
+        curr_len = 0
+        start = 0
+        end = buffsize 
+        decode_error = False
+
+        # Try decoding the entire buffer (self._data_bytes).
+        while curr_len < len(data_bytes) and not decode_error:
+            try:
+                byte_segment = data_bytes[start:end]
+                assert len(byte_segment) <= buffsize
+                decoded_data += str(byte_segment, charset)
+                
+                # Move segment focus
+                start += len(byte_segment)
+                end += len(byte_segment)
+                curr_len += len(byte_segment)
+            
+            except UnicodeDecodeError as e:
+                decode_error = True
+
+        # Get the charset in the already decoded data if error found
+        if not decode_error:
+            assert curr_len == len(data_bytes)
+        
+        else:
+            # Start search for charset subheader, located in Content-Type header.
+            lines = decoded_data.split("\r\n")
+            charset = None
+            curr_line = None
+            i = 0
+            extractor = PatternExtractor()
+            extractor.set_pattern(HTTPResponse.CHARSET_SUBHEADER_CATCHER)
+
+            # Keep cycling through lines until exhausted or charset subheader found
+            while not charset and i < len(lines):
+                curr_line = lines[i]
+                matches = extractor.get_matches(curr_line)
+
+                if matches:
+                    assert len(matches) == 1
+                    charset = matches[0]
+                
+                else:
+                    i += 1
+
+            if charset:
+                decoded_data = str(data_bytes, charset)
+            
+            else:
+                raise UnicodeDecodeError("Could not find decoding scheme of data.")
+            
+        return decoded_data
+    
+    def __repr__(self):
+        return self.content
---
+++ SimpleHTTPFactory.py
@@ -0,0 +1,31 @@
+from twisted.internet.protocol import ClientFactory
+from SimpleHTTP import SimpleHTTP
+from twisted.internet import defer
+from HTTPResponse import HTTPResponse
+
+class SimpleHTTPFactory(ClientFactory):
+    def __init__(self, deferred: defer.Deferred, url: str, scheme: str, 
+                 host: str, path: str, debug: bool):
+        self.scheme = scheme
+        self.url = url
+        self.host = host
+        self.path = path
+        self.debug = debug
+        
+        # Allow callbacks and errbacks
+        self.deferred = deferred
+
+    def buildProtocol(self, addr):
+        return SimpleHTTP(self, self.url, self.scheme, self.host, self.path, self.debug)
+        
+    def http_finished(self, http_response: HTTPResponse):
+        assert isinstance(http_response, HTTPResponse)
+        
+        if self.deferred:
+            d, self.deferred = self.deferred, None
+            d.callback(http_response)
+
+    def http_failed(self, url: str, reason):
+        if self.deferred:
+            d, self.deferred = self.deferred, None
+            d.errback((url, reason))
