d19306b82b0626dea631ddb643f4f592ca7ff631
dapoco <cadizd@proton.me>
2025-01-02 07:14:15 -0600

    Refactored render_POST to handle data of new format.

--- ItemServer.py
+++ ItemServer.py
@@ -24,23 +24,15 @@ class ItemProtocol(Resource):
     ''' HTTP protocol for serving items from the
         supplied item database.'''
     isLeaf = True
-    ITEM_FORMAT_JSON = {"url": "",
-                        "brand": "",
-                        "flavor": "",
-                        "weight": "",
-                        "volume": "",
-                        "count": "",
-                        "company": "",
-                        "manufacturer": "",
-                        "upc": "",
-                        "ean": "",
-                        "asin": "",
-                        "price": ""
-                        }
 
     def __init__(self, file_path, indents=4):
         Resource.__init__(self)
         self._indents = indents
+
+        # Note:
+        # the item database uses the default
+        # dictionary template as described in
+        # ItemDatabase.py.
         self._item_database = ItemDatabase(file_path)
 
     def render_GET(self, request):
@@ -87,22 +79,9 @@ class ItemProtocol(Resource):
         database. Ensure that the client is an
         authenticated user.
 
-        Client passes in a json representing the
-        data of the item in the following format:
-        {"<product_name>": {
-            "url": "",
-            "brand": "",
-            "flavor": "",
-            "weight": "",
-            "volume": "",
-            "count": "",
-            "company": "",
-            "manufacturer": "",
-            "upc": "",
-            "ean": "",
-            "asin": "",
-            "price": "",
-        }'''
+        Client passes in a product json of the format
+        established in item database.
+        '''
         if self._authenticate_request(request):
             # Get the data. Assure that it is json.
             # If json, ensure that it is the correct format
@@ -110,34 +89,27 @@ class ItemProtocol(Resource):
             data = request.content.read().decode("utf-8")
             
             try:
-                data_json = json.loads(data)
+                data_dict = json.loads(data)
 
-                if self._check_format_json(data_json):
-                    # Get name of the item
-                    product_name = list(data_json.keys())[0]
+                # Get the id of the item
+                product_id = data_dict["product_id"]
+                wrote_success = self._item_database.write_data(product_id, data_dict)
 
-                    # Get the values of the item and
-                    # write into the database
-                    values_dict = data_json[product_name]
-                    self._item_database.write_data(product_name=product_name,
-                                                    url=values_dict["url"],
-                                                    brand=values_dict["brand"],
-                                                    flavor=values_dict["flavor"],
-                                                    weight=values_dict["weight"],
-                                                    volume=values_dict["volume"],
-                                                    count=values_dict["count"],
-                                                    company=values_dict["company"],
-                                                    manufacturer=values_dict["manufacturer"],
-                                                    upc=values_dict["upc"],
-                                                    ean=values_dict["ean"],
-                                                    asin=values_dict["asin"],
-                                                    price=values_dict["price"])
-                    return json.dumps(data_json, indent=self._indents).encode("utf-8")
+                if wrote_success:
+                    return json.dumps(data_dict, indent=self._indents).encode("utf-8")
+
+                else:
+                    return b"" # TODO: change code to send back to client
 
             except json.decoder.JSONDecodeError:
                 pass
 
     #------ Helper functions ---------#
+    def _authenticate_request(self,
+                              request: Request):
+        return self._authenticate_api_key(request) or \
+               self._verify_session(request)
+
     def _authenticate_api_key(self, request):
         ''' If the client provided an API key,
         authenticate it.'''
@@ -246,36 +218,6 @@ class ItemProtocol(Resource):
 
             return d
 
-    def _check_format_json(self, dictionary: dict):
-        ''' Check that the dictionary tables
-        the same vanues as required by the
-        item database.'''
-        assert isinstance(dictionary, dict)
-        
-        # The dictionary must be tabled/keyed by the
-        # product name.
-        if len(dictionary) != 1:
-            return False
-    
-        for key in dictionary:
-            if not isinstance(key, str):
-                return False
-
-        # Check that all the values of dictionary follow
-        # the predisclosed format of the class
-        # invariant ITEM_FORMAT_JSON.
-        for key in dictionary:
-            if not isinstance(dictionary[key], dict):
-                return False
-
-            else:
-                sub_dict = dictionary[key]
-
-                for sub_key in sub_dict:
-                    if sub_key not in ItemProtocol.ITEM_FORMAT_JSON:
-                        return False
-        return True
-
 class ItemServer(object):
     DEFAULT_PORT = 1931
     def __init__(self, protocol):
@@ -309,5 +251,5 @@ class ItemServer(object):
         reactor.run()
 
 if __name__ == "__main__":
-    item_server = ItemServer(ItemProtocol("test_file.json"))
+    item_server = ItemServer(ItemProtocol("new_test.json"))
     item_server.run_https(cert_file="crt.pem", key_file="key.pem")
