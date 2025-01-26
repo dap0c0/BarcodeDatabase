70dfaa0873ab27fd7995df9ce2fd37f51049c143
dapoco <cadizd@proton.me>
2025-01-26 12:30:45 -0600

    Fixed bugs

--- RealCanadianPageIterator.py
+++ RealCanadianPageIterator.py
@@ -1,10 +1,11 @@
 import asyncio
 import time
+import sys
 from abc import ABC, abstractmethod
 from DataExtractor import DataExtractor
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-from MongoClient import MongoClientSync, MongoClientAsync
+from MongoClient import MongoClientAsync
 import playwright._impl._errors
 
 class RealCanadianPageIterator(ABC):
@@ -199,8 +200,6 @@ class RealCanadianPageIteratorAsync(RealCanadianPageIterator):
         return context
 
 class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
-    
-
     # TODO: fix hardcoded values for desired departments to crawl.
     async def crawl(self, workers: int):
         root_page = await self._open_page(self._context, None, self._root_url)
@@ -229,8 +228,16 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         # Now, extract the url of all iterable pages (leafs) from these
         # departments and iterate each and every one of them.
         hbb_urls = await self._get_urls_surface(hbb_ul_tag)
+        print(f"Got category urls for CHABA")
+
         jf_urls = await self._get_urls_surface(jf_ul_tag)
+        print("Got category urls for Joe Fresh")
+
+        print("Extracting leafs...")
+        print(f"Extracting leafs from CHABA...")
         hbb_leafs = await self._extract_leafs_department(hbb_urls, workers)
+
+        print(f"Extracting leafs from Joe Fresh...")
         jf_leafs = await self._extract_leafs_department(jf_urls, workers)
 
         # Start extracting products from every product
@@ -255,23 +262,6 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
                 await self._iterate_leaf(page, workers, page_start=1, page_end=None)
 
     # ------ Extraction of each department ------ #
-    async def _extract_leafs_department(self, department_links: list, workers: int):
-        async def foo(leafs_mutable: list, link: str):
-            page = await self._open_page(self._context, None, link)
-            leafs = await self._extract_leafs_no_click(page)
-            leafs_mutable.append(leafs)
-            
-        leafs = []
-
-        while len(department_links) != 0:
-            tasks = []
-
-            for _ in range(workers):
-                tasks.append(foo(leafs, department_links.pop()))
-
-            await asyncio.gather(*tasks)
-        return leafs
-
     async def _get_urls_surface(self, department_tag) -> list:
         '''For the given <department>, extract all level-1 urls.'''
         links = []
@@ -282,10 +272,32 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
 
         return links
 
-    async def _extract_leafs_no_click(self, page) -> list:
-        ''' Return a list of all iterable pages
-        from the surface url.'''
+    async def _extract_leafs_department(self, department_links: list, workers: int):
+        async def foo(leafs_mutable: list, link: str):
+            print(f"Extracting links from {link}")
+            page = await self._open_page(self._context, None, link)
+            leafs = await self._extract_leafs(page)
+            print(f"{len(leafs)} leafs extracted from {link}")
+            await page.close()
+            leafs_mutable.append(leafs)
+            
+        leafs = []
+
+        while len(department_links) != 0:
+            tasks = []
+
+            for _ in range(workers):
+                tasks.append(await foo(leafs, department_links.pop()))
+
+            await asyncio.gather(*tasks)
+        return leafs
+
+    async def _extract_leafs(self, page) -> list:
         assert page != None
+
+        # Base case: the current page is a leaf
+        if await self._is_leaf(page):
+            return [page.url]
         
         # Observe the side accordion list of the page, like in
         # https://www.realcanadiansuperstore.ca/en/baby/c/27987?navid=flyout-L2-Baby.
@@ -296,7 +308,7 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         links = []
         
         for div in accordion_soup.children:
-            assert div.get("class")[0] == "css-srrvm8"
+            assert div.get("class")[0] == "css-srrvm8", breakpoint()
             pulled_list = div.find("ul", class_="css-pc4dq5")
             see_all_tag = pulled_list.contents[0]
             assert see_all_tag.string == "See All"
@@ -307,31 +319,50 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         # leaf is found.
         leafs = []
 
+        # Begin recursion for each link
         for link in links:
             new_page = await self._open_page(self._context, None, link)
-            
-            if await self._check_leaf(new_page):
-                leafs.append(link)
 
-            # Begin recursion
-            else:
-                leafs.append(await self._extract_leafs_no_click(new_page))
-
+            for leaf in await self._extract_leafs(new_page):
+                leafs.append(leaf)
+                
             # Save memory by closing tab
             await new_page.close()
-
         return leafs
+ 
+    async def _is_leaf(self, page) -> bool:
+        # If the page contains this element, it must be a leaf:
+        # <hr aria-orientation="horizontal" class="chakra-divider css-1ap8ayt">
+        await page.wait_for_load_state("domcontentloaded")
+        result_queue = asyncio.Queue()
 
-    async def _check_leaf(self, page) -> bool:
-        ''' Every page with a product grid always
-        has a "Sort by: Relevance" button at the top.
-        A page is a leaf if it contains this sort element.'''
-        try:
-            await page.wait_for_selector("div.css-2wihsd")
-            return True
+        async def positive():
+            try:
+                await page.wait_for_selector("hr.css-1ap8ayt")
+                await result_queue.put(True)
 
-        except playwright._impl._errors.TimeoutError:
-            return False
+            except playwright._impl._errors.TimeoutError:
+                await result_queue.put(False)
+        
+        async def negative():
+            try:
+                # await page.wait_for_selector("div.chakra-accordion.css-8atqhb")
+                await page.wait_for_selector("div.indiana-scroll-container")
+                await result_queue.put(False)
+
+            except playwright._impl._errors.TimeoutError:
+                await result_queue.put(True)
+
+        is_leaf_task = asyncio.create_task(positive())
+        not_leaf_task = asyncio.create_task(negative())
+        result = await result_queue.get()
+        
+        if result:
+            not_leaf_task.cancel()
+
+        else:
+            is_leaf_task.cancel()
+        return result
 
     async def _get_last_page(self, page) -> int | None:
         ''' Return the last iterable page from the
@@ -340,7 +371,7 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         try:
             # Wait for the page navigation to be available
             # at the bottom of the page.
-            await page.wait_for_selector("nav.css-1rb8z0p")
+            await page.wait_for_srelector("nav.css-1rb8z0p")
             html = await page.inner_html("nav.css-1rb8z0p")
             pagination_soup = BeautifulSoup(html, "html.parser")
 
@@ -468,12 +499,6 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         last_page = await self._get_last_page(page)
         assert last_page == None
 
-    # need to find a validt url for this case
-    async def _test_one_page_with_pagination_no_arrow(self):
-        TEST_URL = ""
-        page = await self._open_page(self._context, None, TEST_URL)
-        await self._get_last_page(page)
-
     # passed!
     async def _test_multiple_urls(self):
         urls = ["https://www.realcanadiansuperstore.ca/en/pet-food-supplies/dogs/c/28040",
@@ -489,21 +514,153 @@ class RealCanadianPageIteratorAsyncDiv(RealCanadianPageIteratorAsync):
         tasks = [test_driver(url) for url in urls]
         await asyncio.gather(*tasks)
 
+    async def _test_leafs(self):
+        page = await self._open_page(self._context, None, "https://www.google.com/")
+
+        async def assert_leaf(link: str):
+            page = await self._open_page(self._context, None, link)
+            assert await self._is_leaf(page), f"FAIL: {link} is not a leaf."
+            print(f"PASSED: {link} is a leaf.")
+            await page.close()
+
+        async def assert_not_leaf(link: str):
+            page = await self._open_page(self._context, None, link)
+            assert not await self._is_leaf(page), f"FAIL: {link} is a leaf."
+            print(f"PASSED: {link} isn't a leaf.")
+            await page.close()
+            
+        async def assert_leafs(urls):
+            for url in urls:
+                await assert_leaf(url)
+
+        async def assert_not_leafs(urls):
+            for url in urls:
+                await assert_not_leaf(url)
+
+        async def baby():
+            print("\nTesting baby...")
+
+            async def is_leaf():
+                print("Verifying leaves...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/baby/diapers-wipes-training-pants/c/28030",
+                    "https://www.realcanadiansuperstore.ca/en/baby/nursing-feeding-accessories/c/28031",
+                    "https://www.realcanadiansuperstore.ca/en/baby/pregnancy-maternity/c/28035",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-toiletries/c/28025",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-toys/c/28028",
+                    "https://www.realcanadiansuperstore.ca/en/baby/gear/c/28032",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-safety/c/28026",
+                    "https://www.realcanadiansuperstore.ca/en/baby/car-seats-strollers/c/28029",
+                    "https://www.realcanadiansuperstore.ca/en/baby/gifts/c/28033",
+                    "https://www.realcanadiansuperstore.ca/en/baby/nursery/c/28034",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/cereal/c/46860",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/jars/c/30938",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/pouches/c/30939",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/snacks-biscuits/c/30946",
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/toddler-food/c/30947"
+                ]
+                await assert_leafs(urls)
+
+            async def not_leaf():
+                print("Verifying non-leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/baby/baby-food-snacks/c/42410"
+                ]
+                await assert_not_leafs(urls)
+            await is_leaf()
+            await not_leaf()
+
+        async def joe_fresh():
+            print(f"\nTesting joe fresh...")
+
+            async def is_leaf():
+                print(f"Verifying leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/women/c/56018?navid=flyout-L2-joe-fresh-women&navid=flyout-L2-jf-womens",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/men/c/56216?navid=flyout-L2-joe-fresh-men",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/girls/c/56343?navid=flyout-L2-joe-fresh-girls",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/boys/c/56482?navid=flyout-L2-joe-fresh-boys",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/toddlers/girls/c/56573?navid=flyout-L2-joe-fresh-toddlers",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/toddlers/boys/c/56574?navid=flyout-L2-Toddler%20Boy",
+                    "https://www.realcanadiansuperstore.ca/en/joe-fresh/baby/c/56770?navid=flyout-L2-joe-fresh-baby"
+                ]
+                await assert_leafs(urls)
+
+            async def not_leaf():
+                print(f"Verifying non-leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/collection/jf-seasonal-shops?navid=flyout-L2-jf-seasonal-shops"
+                ]
+                await assert_not_leafs(urls)
+            await is_leaf()
+            await not_leaf()
+
+        async def grocery():
+            print(f"\nTesting grocery...")
+
+            async def is_leaf():
+                print(f"Testing leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/food/meat/sausages/c/28170",
+                    "https://www.realcanadiansuperstore.ca/en/food/meat/sausages/fresh-sausages/c/56935",
+                    "https://www.realcanadiansuperstore.ca/en/floral-shop/c/58821?navid=flyout-L2-FloralShop"
+                ]
+                await assert_leafs(urls)
+
+            async def not_leaf():
+                print(f"Testing non-leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/food/fruits-vegetables/c/28000?navid=flyout-L2-fruits-vegetables",
+                    "https://www.realcanadiansuperstore.ca/en/food/dairy-eggs/c/28003?navid=flyout-L2-Dairy-and-Eggs",
+                    "https://www.realcanadiansuperstore.ca/en/food/meat/c/27998?navid=flyout-L2-Meat",
+                    "https://www.realcanadiansuperstore.ca/en/food/pantry/c/28006?navid=flyout-L2-Pantry",
+                    "https://www.realcanadiansuperstore.ca/en/food/international-foods/c/58044?navid=flyout-L2-International-Foods",
+                    "https://www.realcanadiansuperstore.ca/en/food/snacks-chips-candy/c/57025?navid=flyout-L2-snacks-chips-and-candy",
+                    "https://www.realcanadiansuperstore.ca/en/food/frozen-food/c/28005?navid=flyout-L2-frozen-food",
+                    "https://www.realcanadiansuperstore.ca/en/food/natural-and-organic/c/28189?navid=flyout-L2-Natural-Organic",
+                    "https://www.realcanadiansuperstore.ca/en/food/bakery/c/28002?navid=flyout-L2-Bakery",
+                    "https://www.realcanadiansuperstore.ca/en/food/prepared-meals/c/27996?navid=flyout-L2-Prepared-Meals",
+                    "https://www.realcanadiansuperstore.ca/en/food/drinks/c/28004?navid=flyout-L2-Drinks",
+                    "https://www.realcanadiansuperstore.ca/en/food/deli/c/28001?navid=flyout-L2-Deli",
+                    "https://www.realcanadiansuperstore.ca/en/food/fish-seafood/c/27999?navid=flyout-L2-Fish-and-Seafood",
+                ]
+                await assert_not_leafs(urls)
+            await is_leaf()
+            await not_leaf()
+
+        async def no_items():
+            print(f"\nTesting no items...")
+
+            async def is_leaf():
+                print(f"Testing leafs...")
+                urls = [
+                    "https://www.realcanadiansuperstore.ca/en/lawn-garden-patio/garden-centre/c/28012?navid=flyout-L3-Floral-Garden",
+                    "https://www.realcanadiansuperstore.ca/en/lawn-garden-patio/insect-pest-control/c/28140?navid=CLP-L3-Insect-and-Pest-Control&navid=flyout-L3-Insect-Pest%20Control",
+                    "https://www.realcanadiansuperstore.ca/en/lawn-garden-patio/patio-accessories/c/57036",
+                    "https://www.realcanadiansuperstore.ca/en/lawn-garden-patio/garden-centre/c/28012?navid=flyout-L3-Floral-Garden",
+                ]
+                await assert_leafs(urls)
+            await is_leaf()
+        await baby()
+        await joe_fresh()
+        await grocery()
+        await no_items()
+                    
     async def _test_extract_leafs(self):
         async def no_click():
             async def international():
                 url = "https://www.realcanadiansuperstore.ca/en/food/international-foods/c/58044?navid=flyout-L2-International-Foods"
                 page = await self._open_page(self._context, None, url)
-                leafs = await self._extract_leafs_no_click(page)
+                leafs = await self._extract_leafs(page)
                 print(leafs)
 
             async def baby():
                 url = "https://www.realcanadiansuperstore.ca/en/baby/c/27987?navid=flyout-L2-Baby"
                 page = await self._open_page(self._context, None, url)
-                leafs = await self._extract_leafs_no_click(page)
+                leafs = await self._extract_leafs(page)
                 print(leafs)
 
-            # await international()
+            await international()
             await baby()
         await no_click()
 
