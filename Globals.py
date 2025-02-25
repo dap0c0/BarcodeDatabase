7cc513db3ed0054cdd24bb4d23ed090ac181b683
dapoco <cadizd@proton.me>
2025-02-25 09:08:25 -0600

    Add Department enum for valid departments

--- Globals.py
+++ Globals.py
@@ -1,7 +1,22 @@
 import time
+from enum import Enum
 GROCERY_NAME = "grocery"
 HOME_BEAUTY_BABY_NAME = "home-beauty-baby"
 JF_NAME = "joe-fresh"
 
+class Department(Enum):
+    GROCERY_NAME = "grocery"
+    HOME_BEAUTY_BABY_NAME = "home-beauty-baby"
+    JF_NAME = "joe-fresh"
+
+# TODO: remove this! will be deprecated after
+# reformatting code with DateFormatter
+TIME_FORMAT = "%Y-%m-%d"
+
+# TODO: Remove this!
 def today():
-    return time.strftime("%Y-%m-%d")
+    return time.strftime(TIME_FORMAT)
+
+if __name__ == "__main__":
+    formatter = DateFormatter()
+    print(formatter._date_format)
