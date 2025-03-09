b766508491e9babbb319582204f8a5c291805d02
dapoco <cadizd@proton.me>
2025-03-08 20:05:31 -0600

    Add conversion from json into spreadsheet

--- Globals.py
+++ Globals.py
@@ -9,6 +9,33 @@ class Department(Enum):
     HOME_BEAUTY_BABY_NAME = "home-beauty-baby"
     JF_NAME = "joe-fresh"
 
+class ProductMapping(Enum):
+    ''' Map column index for each field in spreadsheet'''
+    _id = 1
+    product_title = 2
+    product_brand = 3
+    product_url = 4
+    product_id = 5
+    product_package_size = 6
+    price_descriptor = 7
+    regular_price = 8
+    sale_price = 9
+    mop_price = 10
+    non_member_price = 11
+    before_price = 12
+    ean = 13
+    plu = 14
+    upc = 15
+
+    def get(x: str | int):
+        assert isinstance(x, str) or isinstance(x, int)
+        
+        if isinstance(x, str):
+            return ProductMapping.__dict__["_member_map_"][x]
+
+        elif isinstance(x, int):
+            return ProductMapping.__dict__["_value2member_map_"][x]
+
 # TODO: remove this! will be deprecated after
 # reformatting code with DateFormatter
 TIME_FORMAT = "%Y-%m-%d"
@@ -16,7 +43,3 @@ TIME_FORMAT = "%Y-%m-%d"
 # TODO: Remove this!
 def today():
     return time.strftime(TIME_FORMAT)
-
-if __name__ == "__main__":
-    formatter = DateFormatter()
-    print(formatter._date_format)
--- ProductUploader.py
+++ ProductUploader.py
@@ -1,9 +1,13 @@
 from MongoClient import MongoClientAsync
 from abc import ABC, abstractmethod
 import json
+from openpyxl import Workbook
+from Globals import ProductMapping
+from string import ascii_uppercase
 
 class ProductUploader(ABC):
     UPSERT = True
+    DEFAULT_QUERY_FILTER = "_id"
     def __init__(self, isuri: str, database: str, collection: str, filename: str, debug: bool=False):
         assert isinstance(filename, str)
         assert isinstance(isuri, str)
@@ -22,17 +26,123 @@ class ProductUploader(ABC):
     async def get_products(self):
         pass
 
+class SpreadsheetProductUploader(ProductUploader):
+    ''' Allow client to write changes to a spreadsheet
+    and have those changes pushed to the item server.
+
+    This class is a containment of JSONProductUploader, as
+    it is necessary to convert all spreadsheet data
+    into json before uploading.
+    '''
+    DEFAULT_FILENAME_SPREADSHEET = "products.xlsx"
+
+    def __init__(self, isuri: str, database: str, collection: str, filename: str, debug: bool = False):
+        self._json_uploader = JSONProductUploader(isuri, database, collection, debug, filename)
+        self._isuri = isuri
+        self._filename = filename
+        self._debug = debug
+
+    async def push_changes(self, query_filter_name: str=ProductUploader.DEFAULT_QUERY_FILTER):
+        pass
+
+    def get_products(self):
+        workbook = self._json_to_workbook()
+        workbook.save(filename=self.DEFAULT_FILENAME_SPREADSHEET)
+        print(f"Spreadsheet saved as {self.DEFAULT_FILENAME_SPREADSHEET}")
+
+    def _json_to_workbook(self) -> Workbook:
+        ''' Convert all dictionaries from the json file to
+        a workbook.'''
+        workbook = Workbook()
+        sheet = workbook.active
+
+        # Initialize the field row
+        for prod in ProductMapping:
+            assert isinstance(prod.value, int)
+            assert isinstance(prod.name, str)
+            sheet[f"{self._int_to_alpha(prod.value)}1"] = prod.name
+
+        # Append new products to the sheet
+        curr_row = 2
+        products_dict = self._get_products_json()
+
+        for _, product in products_dict.items():
+            for key, val in self._denest_dict(product).items():
+                if isinstance(val, str):
+                    column = ProductMapping.get(key).value
+                    sheet[f"{self._int_to_alpha(column)}{curr_row}"] = val
+
+            curr_row += 1
+        return workbook
+
+    def _workbook_to_dict(self, workbook: Workbook) -> dict:
+        assert isinstance(workbook, Workbook)
+
+    def _int_to_alpha(self, val: int) -> str:
+        ''' Convert the integer value to its respective
+        alphanumeric string. E.g.:
+        1 -> "A"
+        2 -> "B"
+        26 -> "Z"
+        27 -> "AA"
+        28 -> ""AB"
+        53 -> "BA"
+
+        Treat the strings as if they were a base-26 system.'''
+        assert isinstance(val, int)
+        
+        # TODO: provide functionality for int vals past 26
+        if val >= 1:
+            return ascii_uppercase[val - 1]
+
+        raise ValueError(f"{val} isn't >= 1")
+
+    def _denest_dict(self, dictionary: dict) -> dict:
+        ''' Denest all dictionaries within the given dictionary and
+            return a dictionary with all fields of non-dictionary
+            class.'''
+        assert isinstance(dictionary, dict)
+        dicts_found = []
+        denested_dict = {}
+
+        # Iterate through each field to check if
+        # a nested dictionary is present.
+        for key, item in dictionary.items():
+            if isinstance(item, dict):
+                dicts_found.append(item)
+
+            else:
+                denested_dict[key] = item
+
+        # Base case: the current dictionary doesn't contain
+        # any nested dictionaries. Denested_dict is already
+        # populated with the same fields as the original dictionary.
+        if len(dicts_found) == 0:
+            return denested_dict
+
+        # Recursive case: the current dictionary contains >= 1
+        # dictionary items.
+        else:
+            [denested_dict.update(self._denest_dict(d)) for d in dicts_found]
+            return denested_dict
+            
+    def _get_products_json(self) -> dict:
+        ''' Read from the json file and return
+        all products in a dictionary.'''
+        with open(self._filename, "r") as rfile:
+            data_dict = json.load(rfile)
+            return data_dict
+
 class JSONProductUploader(ProductUploader):
-    ''' Allow client to write changes to a file
+    ''' Allow client to write changes to a json file
     and have those changes pushed to the item server.'''
 
-    DEFAULT_QUERY_FILTER = "_id"
     DEFAULT_OUTPUT_FILENAME = "products.json"
     DEFAULT_INDENT = 4
-    def __init__(self, isuri: str, database: str, collection: str, debug: bool=False, filename: str=DEFAULT_OUTPUT_FILENAME,):
+    def __init__(self, isuri: str, database: str, collection: str, debug: bool=False, filename: str=DEFAULT_OUTPUT_FILENAME):
         ProductUploader.__init__(self, isuri, database, collection, filename, debug)
 
-    async def push_changes(self, query_filter_name: str=DEFAULT_QUERY_FILTER):
+    async def push_changes(self, query_filter_name: str=ProductUploader.DEFAULT_QUERY_FILTER):
         assert isinstance(query_filter_name, str)
         assert query_filter_name != ""
 
@@ -68,3 +178,5 @@ class JSONProductUploader(ProductUploader):
 
             if self._debug:
                 print(f"Dumped json to {self._filename}")
+
+uploader.get_products()
