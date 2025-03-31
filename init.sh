ada9f2b94ed3d2c41c5608dfa720c33f8e3edeba
dapoco <cadizd@proton.me>
2025-03-31 15:05:48 -0500

    refactor: change file paths to account for reorganization of directory structure

--- init.sh
+++ init.sh
@@ -11,8 +11,9 @@ BASH_PATH=$(which bash)
 DOCKER_PATH=$(which docker)
 PROJECT_PATH=$(pwd)
 CRONFILE_NAME=cronfile
-ISURI_FILE=item_server_url.txt
-CLOUDFLARE_TUNNEL_TOKEN_FILE=cf_tunnel_token.txt
+CONFIG_PATH=$PROJECT_PATH/config
+ISURI_PATH=$CONFIG_PATH/item_server_url.txt
+CLOUDFLARE_TUNNEL_TOKEN_PATH=$CONFIG_PATH/cf_tunnel_token.txt
 
 # ----- Backend ----- #
 # Normal crawl vars
@@ -21,7 +22,7 @@ CLOUDFLARE_TUNNEL_TOKEN_FILE=cf_tunnel_token.txt
 NUM_CRAWLS_PER_DAY=10
 CRAWL_START_HOUR=2
 CRAWL_START_MINUTE=0
-CRAWLER_COMPOSE_FILE=compose.rc_crawler.yaml
+BACKEND_COMPOSE_PATH=$PROJECT_PATH/compose.backend.yaml
 CRAWLER_SERVICE=rc_crawler
 LEAF_EXTRACTOR_SERVICE=leaf_extractor
 
@@ -35,7 +36,7 @@ LEAF_EXTRACTION_START_MINUTE=0
 # ----- Frontend ----- #
 # By default, start the frontend server
 # at 6:45 am.
-FRONTEND_COMPOSE_FILE=compose.yaml
+FRONTEND_COMPOSE_PATH=$PROJECT_PATH/compose.frontend.yaml
 FRONTEND_START_HOUR=6
 FRONTEND_START_MINUTE=45
 
@@ -57,7 +58,7 @@ touch $CRONFILE_NAME
 echo "Instructing crontab to initiate crawl $NUM_CRAWLS_PER_DAY times per day" \
   "starting at $CRAWL_START_HOUR:$CRAWL_START_MINUTE"
 echo "$CRAWL_START_MINUTE $CRAWL_START_HOUR * * *" \
-  "$DOCKER_PATH compose -f $PROJECT_PATH/$CRAWLER_COMPOSE_FILE run $LEAF_EXTRACTOR_SERVICE" >$CRONFILE_NAME
+  "$DOCKER_PATH compose -f $BACKEND_COMPOSE_PATH run $LEAF_EXTRACTOR_SERVICE" >$CRONFILE_NAME
 
 # Backend crawl. Perform
 # for the predesignated amount of times
@@ -65,21 +66,21 @@ echo "Instructing crontab to extract leaves on day $LEAF_EXTRACTION_DAY" \
   "starting at $LEAF_EXTRACTION_START_HOUR:$LEAF_EXTRACTION_START_MINUTE"
 echo "$LEAF_EXTRACTION_START_MINUTE $LEAF_EXTRACTION_START_HOUR * * $LEAF_EXTRACTION_DAY" \
   "$BASH_PATH -c \"for i in {1..$NUM_CRAWLS_PER_DAY}:; do" \
-  "$DOCKER_PATH compose -f $PROJECT_PATH/$CRAWLER_COMPOSE_FILE run $CRAWLER_SERVICE; done\"" >>$CRONFILE_NAME
+  "$DOCKER_PATH compose -f $BACKEND_COMPOSE_PATH run $CRAWLER_SERVICE; done\"" >>$CRONFILE_NAME
 
 # Daemonize the frontend server
 # at designated time
 echo "Instructing crontab to fireup server everyday" \
   "from $FRONTEND_START_HOUR:$FRONTEND_START_MINUTE"
 echo "$FRONTEND_START_MINUTE $FRONTEND_START_HOUR * * *" \
-  "$DOCKER_PATH compose -f $PROJECT_PATH/$FRONTEND_COMPOSE_FILE up -d" >>$CRONFILE_NAME
+  "$DOCKER_PATH compose -f $FRONTEND_COMPOSE_PATH up -d" >>$CRONFILE_NAME
 
 # Stop the frontend server
 # at designated time
 echo "Instructing crontab to stop server everday" \
   "at $FRONTEND_STOP_HOUR:$FRONTEND_STOP_MINUTE"
 echo "$FRONTEND_STOP_MINUTE $FRONTEND_STOP_HOUR * * *" \
-  "$DOCKER_PATH compose -f $PROJECT_PATH/$FRONTEND_COMPOSE_FILE down" >>$CRONFILE_NAME
+  "$DOCKER_PATH compose -f $FRONTEND_COMPOSE_PATH down" >>$CRONFILE_NAME
 
 # Append this cronfile's contents to
 # the current user's crontab
@@ -90,5 +91,5 @@ rm $CRONFILE_NAME
 
 # Touch files that should be used for secrets
 echo "Touching secret files..."
-touch $ISURI_FILE && echo "ISURI FILE: $ISURI_FILE"
-touch $CLOUDFLARE_TUNNEL_TOKEN_FILE && echo "CF TUNNEL FILE: $CLOUDFLARE_TUNNEL_TOKEN_FILE"
+touch $ISURI_PATH && echo "ISURI PATH: $ISURI_PATH"
+touch $CLOUDFLARE_TUNNEL_TOKEN_PATH && echo "CF TUNNEL PATH: $CLOUDFLARE_TUNNEL_TOKEN_PATH"
